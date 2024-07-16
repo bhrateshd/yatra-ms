4e02b60c2e761f9c2bc2ae827c3cfb22c49b752b
Bhratesh <bhrateshd@gmail.com>
2024-07-16 21:18:02 +0530

    adding new file/code

--- src/main/java/com/example/yatra_ms/railways.java
+++ src/main/java/com/example/yatra_ms/railways.java
@@ -1,4 +1,3 @@
-
 package com.example.yatra_ms;
 
 
@@ -8,5 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class railways {
     @GetMapping("/railways")
-    public String getData() {return  "Please book your railways ticket from yatra app on  30% discount" ; }
+    public String getData() {return  "Please book your railways ticket from yatra app on  40% discount" ; }
 }
