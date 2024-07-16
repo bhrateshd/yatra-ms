16787ad35319d647c9b7452523fba0579cd7b05e
Bhratesh <bhrateshd@gmail.com>
2024-07-16 10:01:27 +0530

    adding in trest class

--- src/test/java/com/example/yatra_ms/YatraMsApplicationTests.java
+++ src/test/java/com/example/yatra_ms/YatraMsApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.yatra_ms;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 @SpringBootTest
 class YatraMsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Ensure that the application context loads successfully
 	}
-
 }
+
